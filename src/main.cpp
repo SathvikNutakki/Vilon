4673dd4fb3f22900827c55149918fd2929de7e1a
SathvikNutakki <sathviknutakki9@gmail.com>
2026-05-25 13:25:08 +0530

    UART pipleline check

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,15 @@
 #include <Arduino.h>
 
 void setup(){
-  ledcSetup(0,5000,8);
-  ledcAttachPin(2,0);
   Serial.begin(115200);
-
-
+  
 }
 void loop(){
-  
-  for (int k=0;k<3;k++)
-  {
-    for(int i=0;i<255;i++){
-      ledcWrite(0,i);
-      delay(10);
-      
-    }
-    delay(100);
-    for(int i=255;i>=0;i--){
-      ledcWrite(0,i);
-      delay(10);  
-    }
-    
-    
-
+  if (Serial.available()){
+     String incoming = Serial.readStringUntil('\n');
+     Serial.println(incoming);
   }
-  while(true);
   
+
+
 }
