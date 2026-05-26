e8c94239baa6038235e46fdbea297b267efff899
SathvikNutakki <sathviknutakki9@gmail.com>
2026-05-26 09:23:42 +0530

    UV SENSOR TESTING

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,38 @@
 #include <Arduino.h>
+#define TRIG_PIN 5
+#define ECHO_PIN 18
+
+
 
 void setup(){
   Serial.begin(115200);
+  pinMode(TRIG_PIN,OUTPUT);
+  pinMode(ECHO_PIN,INPUT);
+  pinMode(LEFT_IR,INPUT);
+  pinMode(RIGHT_IR,INPUT);
   
 }
 void loop(){
-  if (Serial.available()){
-     String incoming = Serial.readStringUntil('\n');
-     incoming.trim();
-     if (incoming == "FORWARD"){
-        Serial.println("Moving Forward...");
-     }
-     else if (incoming == "STOP" ){
-      Serial.println("Stopping.");
-     }
-     else if (incoming == "STATUS"){
-      Serial.println("All systems nominal.");
-     }
-     else{
-      Serial.println("Unknown command:" + incoming);
-     }
-     
+  //for screaming
+  digitalWrite(TRIG_PIN,LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN,LOW);
+  //for hearing the echo
+  long duration = pulseIn(ECHO_PIN,HIGH);
+  float distance = (duration*0.034)/2;
+  if (distance<20){
+    Serial.println("OBSTACLE DETECTED!!");
+  
   }
+  else{
+    Serial.print("DISTANCE: ");
+    Serial.print(distance);
+    Serial.println("CM");
+  }
+  delay(500);
   
 
-
 }
+
